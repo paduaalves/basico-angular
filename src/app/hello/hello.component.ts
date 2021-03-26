6ac9e8f00b75fde93ee5fada8167e6c05933afaa
Pádua Alves <paduaalves03@gmail.com>
2021-03-26 10:09:57 -0300

    Imprimindo atributos dinamicamente

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -2,10 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
     selector : 'hello',
-    template : `
-        <p>Hello World</p>
-    `
+    templateUrl : './hello.component.html'
 })
 export class HelloComponent {
     
+    nome : string;
+    clientes : Cliente[];
+
+    constructor(){
+        this.nome = 'Pádua';
+        let fulano = new Cliente("Fulano", 30);
+        let cicrano = new Cliente("cicrano", 20);
+        let outro = new Cliente("outro", 32);
+        this.clientes = [fulano, cicrano, outro];
+    }
+
+}
+
+class Cliente {
+    constructor(public nome: string, 
+                public idade: number
+        ){
+
+    }
 }
